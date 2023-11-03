e7d0ec3b04c0c5705e2ebbc6ca2f0ecdfe60f187
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-03 10:33:41 -0300

    Add files via upload

---
+++ SimpleNavigationSwiftUI/SimpleNavigationSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleNavigationSwiftUI
+//
+//  Created by Lucas Neves dos santos pompeu on 03/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleNavigationSwiftUI/SimpleNavigationSwiftUI/SimpleNavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNavigationSwiftUIApp.swift
+//  SimpleNavigationSwiftUI
+//
+//  Created by Lucas Neves dos santos pompeu on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
