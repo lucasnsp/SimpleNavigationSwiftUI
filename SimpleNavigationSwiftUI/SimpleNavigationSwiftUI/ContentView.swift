30b77900ef9b8f2807868efc0474b48e4755aed2
Lucas Neves <lucas.neves12@icloud.com>
2023-11-03 11:23:42 -0300

    Learning how to do a simpleNavigation with FullScreenCover and Sheet.

--- SimpleNavigationSwiftUI/SimpleNavigationSwiftUI/ContentView.swift
+++ SimpleNavigationSwiftUI/SimpleNavigationSwiftUI/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented = false
+    @State private var isPresentedFullScreen = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack (spacing: 20.0) {
+                Button("Apresentar Sheet") {
+                    isPresented.toggle()
+                }
+                .sheet(isPresented: $isPresented) {
+                    SheetView()
+                }
+                Button("Apresentar FullScreen") {
+                    isPresentedFullScreen.toggle()
+                }
+                .fullScreenCover(isPresented: $isPresentedFullScreen) {
+                    SheetView()
+                }
+            }
+            
         }
         .padding()
+    }
+}
+
+struct SheetView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea(.all)
+            VStack {
+                Text("Deu bom")
+                Button("Voltar") {
+                    dismiss()
+                }
+            }
+        }
     }
 }
 
